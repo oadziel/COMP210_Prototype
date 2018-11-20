5e9838a796bca37860b5a613bbc05644f9406914
oadziel <josh@jlewisgames.co.uk>
2018-11-20 12:15:11 +0000

    basic drop added

---
+++ COMP210_Prototype/Assets/Scripts/CS_Box_Drop.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CS_Box_Drop : MonoBehaviour {
+    [SerializeField] private GameObject dropObject;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit rayhit;
+            if (Physics.Raycast(ray, out rayhit))
+            {
+                Vector3 Raydrop = new Vector3(rayhit.transform.position.x, rayhit.transform.position.y + 10.0f, rayhit.transform.position.z);
+                Instantiate(dropObject, Raydrop, transform.rotation);
+            }
+        }
+    }
+}
